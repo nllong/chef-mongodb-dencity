6c27d8b88a5eaaa20d0e08cf7c3dfd7505c21ff5
Nicholas Long <nicholas.long@nrel.gov>
2014-06-27 15:22:03 -0600

    initial mongodb instance

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'mongodb-dencity'
+maintainer       'Nicholas Long'
+maintainer_email 'nicholas.long@nrel.gov'
+license          'All rights reserved'
+description      'Installs/Configures mongodb-dencity'
+long_description 'Installs/Configures mongodb-dencity'
+version          '0.1.0'
+
+depends 'mongodb'
---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: mongodb-dencity
+# Recipe:: default
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "mongodb::10gen_repo"
+include_recipe "mongodb::default"
